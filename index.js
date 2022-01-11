4ff53ce571574639ad786938ec66272dba3d5983
XKaliber <srihariguy@gmail.com>
2022-01-12 00:39:26 +0530

    Js Base bot

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 //Pre-requisites
 const {Client, Intents, MessageActionRow, MessageAttachment} = require('discord.js');
 require("dotenv").config();
+const fs = require('fs');
+var userIDs= JSON.parse(fs.readFileSync('./resources/userIDs.json'));
+var possibleMsgs = JSON.parse(fs.readFileSync('./resources/possibleMsgs.json'));
+var responses = JSON.parse(fs.readFileSync('./resources/responses.json'));
 
 const client = new Client({
     intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
@@ -9,6 +13,7 @@ const client = new Client({
 //Set Bot Secrets and Prefix
 const token = process.env.DISCORD_TOKEN
 const prefix = "xkali";
+
 
 //Bot Initiation Prompt
 client.on("ready", () => {
@@ -30,10 +35,13 @@ client.on("ready", () => {
 
 client.on("messageCreate", (message) => {
     
+    //Check if it's me or my fellow bots.
     if (message.author.bot) return;
     
+    //Convert all text to lowercase
     msg = message.content.toLowerCase();
 
+    //Welcome reaction
     if (msg.startsWith("wannacum") || msg.startsWith('whalecum')) 
     {
       const squidward = new MessageAttachment('https://c.tenor.com/2zEJyyQy3GEAAAAC/daring-today-are-we-spongebob.gif');
@@ -41,22 +49,49 @@ client.on("messageCreate", (message) => {
       message.reply( {content:"Yo! Vannakam!\nFeeling kinky today, aren't we OwO", files : [squidward] } );
     }
 
-    if (msg.startsWith(asuka_hello_trigger))
-    {
+   // if (msg.startsWith(asuka_hello_trigger))
+    //{
 
-        const asuka_hello = new MessageAttachment('./resources/media/asuka says hello.mp4');
-        message.channel.sendTyping(5);
-        message.reply({content:'Halo!',files : [asuka_hello]});
+     //   const asuka_hello = new MessageAttachment('./resources/media/asuka says hello.mp4');
+     //   message.channel.sendTyping(5);
+     //   message.reply({content:'Halo!',files : [asuka_hello]});
+   // }
+
+    //Bot Trigger when Mentioned
+    if (message.mentions.has(client.user) && message.author.id != userIDs.master)
+    { 
+        const tagTrigger = Math.floor(Math.random() * responses.botTag.length)
+        message.reply(responses.botTag[tagTrigger])
     }
 
-    if (msg.startsWith('i iz noob') && message.author.id == client.users.cache.find(u => u.tag === 'master').id)
+    //Shinobu appreciation
+    if (possibleMsgs.shinobu.includes(msg) == true)
     {
-        message.reply("It's okay master")
+        const shinobuTrigger = Math.floor(Math.random() * responses.shinobu.length)
+        message.reply(responses.shinobu[shinobuTrigger])
     }
 
+    //When someone acts dumb
+    if (msg.startsWith("probably") && message.author.id != userIDs.master)
+    {
+        message.reply("Yes or No, No probably dip shit plz.")
+    }
+    else if (msg.startsWith("probably") && message.author.id == userIDs.master) //When master isn't sure.
+    {
+        message.reply("It\'s okay Master therinja matum sollunga 😌✨")
+    }
 
-
-  
+    //When master doesn't know a thing
+    if (possibleMsgs.noob.includes(msg) == true && message.author.id == userIDs.master)
+    {
+        const hanekawaCatchPhrase = new MessageAttachment('./resources/media/noob.jpg');
+        message.reply({content:"Master doesn\'t know everything, He only knows what he knows.", files :[hanekawaCatchPhrase]});
+    }
+    else if(possibleMsgs.noob.includes(msg) == true && message.author.id != userIDs.master)
+    {
+        const noobTrigger = Math.floor(Math.random() * responses.noob.length)
+        message.reply(responses.noob[noobTrigger])
+    }
 
 });
 
